d0ada101bb8ef60c76f58a5771e88864d8d408cd
Sivakumar Rajachidambaram <sivaram.ec@gmail.com>
2020-07-01 22:39:03 +0530

    Create BestTime_to_buyandsell_stock_1_128.cpp

---
+++ BestTime_to_buyandsell_stock_1_128.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=INT_MIN,sell=0;
+        
+        for(int i=0;i<prices.size();i++){
+            buy = max(buy, -prices[i]);
+            sell = max(sell,buy+prices[i]);
+            
+            cout << prices[i] << " " << buy <<" " << sell<<"\n";
+        }
+        
+        return sell;
+    }
+};
