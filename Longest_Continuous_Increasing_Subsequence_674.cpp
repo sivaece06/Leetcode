225fae17a076048bebcacd9d1121355ae665cdfe
Sivakumar Rajachidambaram <sivaram.ec@gmail.com>
2020-07-20 22:18:45 +0530

    Update Longest_Continuous_Increasing_Subsequence_674.cpp

--- Longest_Continuous_Increasing_Subsequence_674.cpp
+++ Longest_Continuous_Increasing_Subsequence_674.cpp
@@ -25,3 +25,34 @@ public:
         return max_m;
     }
 };
+
+/* Two pointer approach */
+
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        
+      int len = nums.size();
+    
+      if (len == 0)
+          return 0;
+        
+      if (len == 1)
+          return 1;
+        
+      int result = 1, start = 0, end = 1;
+      
+      while (end < len) {
+          
+          if (nums[end - 1] < nums[end]) {
+              result = max(result, end-start+1);
+          } else {
+              start = end;
+          }
+          
+          end++;
+      }
+    
+    return result;
+    }
+};
