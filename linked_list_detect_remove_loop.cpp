71315184a751f7de41ba68d77782608605894db0
Sivakumar Rajachidambaram <sivaram.ec@gmail.com>
2021-05-26 17:10:15 +0530

    Update linked_list_detect_remove_loop.cpp

--- linked_list_detect_remove_loop.cpp
+++ linked_list_detect_remove_loop.cpp
@@ -1,3 +1,22 @@
+/**
+Detect a Loop:
+-------------
+Point slow and fast pointer to the first node where head is pointing to.
+Continue to move slow pointer by one node (slow = slow->next) and fast pointer by two nodes (fast = fast->next->next).
+If at any point slow or fast becomes NULL, stop the traversal. There is no loop in the linked list.
+If at any point, slow and fast meet together (slow == fast), there is a loop in the list.
+
+Remove the Loop:
+--------------
+To remove the loop, we need to figure out the last node of the list and point that node to NULL. In the above algorithm, 
+slow and fast pointers meet somewhere in the loop which might not be the start or end node of the loop.
+
+Point the near pointer to the first node. And point the far pointer as far as the size of the loop. Keep a prev pointer just before far pointer.
+Keep moving above three pointers, until near meets far. The meeting point would be the start of the loop and prev would be the end of the loop.
+Assign NULL to prev.
+Note: To place the far pointer as far as the size of the loop, we can first point the far pointer to the first node. Point another pointer,
+say ptr, to next of any loop node (say where slow and fast met). Keep moving far and ptr until ptr comes back to the loop node.
+**/
 class Solution
 {
     public:
