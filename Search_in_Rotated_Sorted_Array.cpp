7e129d63b1986214e7cfeae25403535afc3350fd
Sivakumar Rajachidambaram <sivaram.ec@gmail.com>
2021-06-06 11:39:17 +0530

    Create Search_in_Rotated_Sorted_Array.cpp

---
+++ Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,62 @@
+/**
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+**/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1, mid;
+        
+        while (start <= end) {
+            
+            mid = start + (end-start) / 2;
+            // mid = (start + end) / 2;
+            
+            if (nums[mid] == target)
+                return mid;
+            
+            if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = start + 1;
+                
+            } else {
+                
+                if (target > nums[mid] && target <= nums[end])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+                
+            }
+            
+        }
+        
+        return -1;
+    }
+};
