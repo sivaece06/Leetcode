6725176344558b2f8ec9869ba5c61336330a8d04
Sivakumar Rajachidambaram <sivaram.ec@gmail.com>
2022-03-05 22:36:43 +0530

    Create squares_of_sorted_array.cpp

---
+++ squares_of_sorted_array.cpp
@@ -0,0 +1,46 @@
+/*
+
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+*/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> res(n);
+        
+        int square;
+        int left = 0;
+        int right =  n - 1;
+        
+        for (int i = n-1; i>=0; i--) {
+            int sqaure;
+            
+            if (abs(nums[left]) > abs(nums[right])) {
+                square = nums[left];
+                left++;
+            } else {
+                square = nums[right];
+                right--;
+            }
+            
+            res[i] = square * square;
+        }
+        
+        return res;
+    }
+};
